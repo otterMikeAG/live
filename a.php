a0d4bcf1049034c735bb5bd62ccca12021b2376a
Michael Avalos Garcia <mavalos-garcia@csumb.edu>
2018-12-12 20:42:05 +0000

    merge

---
+++ a.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+* DATABASE LOCAL CONFIG
+**/
+
+$_ENV['DB_HOST'] = 'localhost';
+$_ENV['DB_NAME'] = 'live';
+$_ENV['DB_USERNAME'] = 'live_administrator';
+$_ENV['DB_PASSWORD'] = 'GznUDh1AN8n60JlE';
+
+/**
+* MAILJET API KEYS
+**/
+$_ENV['MAILJET_HOST'] = 'in-v3.mailjet.com';
+$_ENV['MAILJET_PUBLIC_KEY'] = 'b2f893864f3d97b65f0d749c43182770';
+$_ENV['MAILJET_PRIVATE_KEY'] = '35af6ebef771640a42fa7e2ce5457b47';
+$_ENV['EMAIL_SENDER'] = 'liveappmailer@gmail.com';
+
+/**
+* GOOGLE MAPS API KEY
+**/
+
+$_ENV['GOOGLE_MAPS_API_KEY'] = 'AIzaSyDKlOpnC8ZKf2LN1Q6MoP3ZS4TmGTGYeho';
+
+?>
